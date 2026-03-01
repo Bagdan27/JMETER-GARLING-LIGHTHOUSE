454f29e0883d062f59832ed6ef06a7c192984c0a
Bohdan <160043493+Bagdan27@users.noreply.github.com>
2026-03-01 17:54:32 +0200

    Update BaseHelpers.scala

--- gating/src/test/scala/helpers/BaseHelpers.scala
+++ gating/src/test/scala/helpers/BaseHelpers.scala
@@ -4,15 +4,10 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
 object BaseHelpers {
-  val appUrl        = System.getProperty("baseUrl", "http://localhost")
-  val userCount     = Integer.getInteger("users", 5).toInt
-  val rampDuration  = Integer.getInteger("ramp", 60).toInt
-  val testDuration  = Integer.getInteger("duration", 300).toInt 
-
-  val commonHeaders = Map(
-    "Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "User-Agent" -> "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-  )
+  val appUrl        = System.getProperty("baseUrl",  "http://localhost")
+  val userCount     = Integer.getInteger("users",    5)
+  val rampDuration  = Integer.getInteger("ramp",     30)
+  val testDuration  = Integer.getInteger("duration", 180)  // ← ДОБАВЛЕНО: длительность теста после ramp-up
 
   val httpProtocol = http
     .baseUrl(appUrl)
@@ -20,6 +15,7 @@ object BaseHelpers {
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+    .warmUp(appUrl)                          // ← прогрев соединения перед тестом
 
   def thinkTime(min: Int = 2, max: Int = 5) = pause(min, max)
 }
