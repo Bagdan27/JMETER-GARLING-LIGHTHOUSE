e31669f32f108f75cb44bfe4878433e5e68e0025
Bohdan <160043493+Bagdan27@users.noreply.github.com>
2026-03-02 17:05:58 +0200

    Refactor EcommerceSimulation setup and add maxDuration
    Removed comments in the EcommerceSimulation setup and added maxDuration.

--- gating/src/test/scala/simulations/EcommerceSimulation.scala
+++ gating/src/test/scala/simulations/EcommerceSimulation.scala
@@ -6,17 +6,14 @@ import scenarios.UserScenarios
 import scala.concurrent.duration._
 
 class EcommerceSimulation extends Simulation {
-
   setUp(
     UserScenarios.scn.inject(
-      // 1. Разгоняем нагрузку от 0 до 50 ОДНОВРЕМЕННЫХ пользователей за 120 сек
       rampConcurrentUsers(0).to(userCount).during(rampDuration.seconds),
-
-      // 2. Удерживаем ровно 50 ОДНОВРЕМЕННЫХ пользователей в течение 180 сек
       constantConcurrentUsers(userCount).during(testDuration.seconds)
     )
   )
   .protocols(httpProtocol)
+  .maxDuration(totalDuration.intValue.seconds)  // ← явный unbox
   .assertions(
     global.responseTime.percentile(95).lt(5000),
     global.successfulRequests.percent.gt(95)
